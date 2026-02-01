70b1fc8a53a9b3515528c064ae2eb7cff2cd21f4
Martin <mvondermey@gmail.com>
2026-02-01 15:19:18 +0100

    Update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,6 +18,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/schmerzfreie-laser-haarentfernung',
     '/impressum',
     '/datenschutz',
+    '/agb',
   ]
 
   return routes.map((route) => ({
