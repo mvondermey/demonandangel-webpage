7984b78fdc98e62f59c27a3299fb68b952361950
Martin <mvondermey@gmail.com>
2026-02-01 13:55:16 +0100

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,17 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P4RLPXHB');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -155,8 +166,18 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P4RLPXHB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
-        <main className="pt-24 lg:pt-32">{children}</main>
+        <main className="pt-56 lg:pt-64">{children}</main>
         <Footer />
       </body>
     </html>
